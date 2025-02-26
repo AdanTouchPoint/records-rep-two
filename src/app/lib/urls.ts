7b9750001e08d51242b8dafd27a52e8c766e5281
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2025-02-26 15:20:33 -0600

    edited: endpoint urls

--- src/app/lib/urls.ts
+++ src/app/lib/urls.ts
@@ -1,7 +1,7 @@
 const Urls = {
-    electoratesByCp: process.env.NEXT_PUBLIC_HOST ? `${process.env.NEXT_PUBLIC_HOST}dashboardContent/electorates-record/` : 'http://localhost:8080/dashboardContent/electorates-record/',
+    electoratesByCp: process.env.NEXT_PUBLIC_HOST ? `${process.env.NEXT_PUBLIC_HOST}dashboardContent/custom-electorates-v2/` : 'http://localhost:8080/dashboardContent/custom-electorates-v2/',
     //reps: process.env.HOST || 'http://localhost:8080/dashboardContent/reps-by-elects/',
-    repsByElectorate:  process.env.NEXT_PUBLIC_HOST ? `${process.env.NEXT_PUBLIC_HOST}dashboardContent/reps-record/` : 'http://localhost:8080/dashboardContent/reps-record/',
+    repsByElectorate:  process.env.NEXT_PUBLIC_HOST ? `${process.env.NEXT_PUBLIC_HOST}dashboardContent/custom-representatives-v2/` : 'http://localhost:8080/dashboardContent/custom-representatives-v2/',
     leadsCreate:  process.env.NEXT_PUBLIC_HOST ? `${process.env.NEXT_PUBLIC_HOST}dashboardContent/leads/` : 'http://localhost:8080/dashboardContent/leads/'
 }
 export { Urls }
